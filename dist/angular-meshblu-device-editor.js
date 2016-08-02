414bf3b908333d7ab417437532c18d91ea261e90
Aaron Herres <a@octoblu.com>
2016-08-02 13:07:28 -0700

    v3.9.15 Ok. committing compiled files is not good. This is driving me crazy

--- dist/angular-meshblu-device-editor.js
+++ dist/angular-meshblu-device-editor.js
@@ -237,9 +237,9 @@
   _ = window._, angular = window.angular, jsonSchemaDefaults = window.jsonSchemaDefaults;
 
   MeshbluSchemaFormController = (function() {
-    function MeshbluSchemaFormController(scope1) {
+    function MeshbluSchemaFormController(scope) {
       var base;
-      this.scope = scope1;
+      this.scope = scope;
       this.isEmpty = bind(this.isEmpty, this);
       this.formSchema = bind(this.formSchema, this);
       this.selectSchema = bind(this.selectSchema, this);
@@ -253,7 +253,7 @@
 
     MeshbluSchemaFormController.prototype.selectSchema = function() {
       var defaults;
-      console.log('selectSchema', scope);
+      console.log('selectSchema', this.scope);
       if (!((this.scope.schemas != null) && (this.scope.formSchemas != null) && (this.scope.selectedSchemaKey != null))) {
         return;
       }
